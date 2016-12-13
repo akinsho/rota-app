8277c1dc21bab9be8b4d28d4b6bfd5af88998ab1
Akin909 <akin.sowemimo@gmail.comgit config --global user.name Akin909ls git config --global user.email akin.sowemimo@gmail.com>
2016-12-13 13:23:34 +0000

    add app.jsx file

---
+++ app/components/app.jsx
@@ -0,0 +1,21 @@
+var React = require( 'react' );
+var ReactDOM = require( 'react-dom' );
+var {Route, Router, IndexRoute, hashHistory}= require("react-router")
+var Main = require ('Main')
+
+
+// //Load foundation
+// require('style!css!foundation-sites/dist/foundation.min.css')
+// $(document).foundation();
+// //App css
+// require("style!css!sass!applicationStyles")
+
+var routes = (<Router history={hashHistory}>
+  <Route path = "/" component = {Main}>
+
+  </Route>
+</Router>);
+
+ReactDOM.render(routes,
+document.getElementById( 'app' )
+);
---
+++ public/components/navigation.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var navigation = React.createClass({
+  render: function(){
+    return(
+      <div>
+        Navigation
+      </div>
+    )
+  }
+})
